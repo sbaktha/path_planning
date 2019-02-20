9ca2c740f1f4ef9daa0bbdb6967a44a15be225b5
yesbeekay <kolahalvsl@gmail.com>
2019-02-20 12:17:34 +0530

    Changed goal type in rviz from point to Pose (move_base_simple_goal)

--- src/elevate_goal_point.cpp
+++ src/elevate_goal_point.cpp
@@ -1,23 +1,25 @@
 #include "ros/ros.h"
 #include "geometry_msgs/PointStamped.h"
-
+#include "geometry_msgs/PoseStamped.h"
 class GoalShifter
 {
 public:
   GoalShifter()
   {
     pub_ = nh_.advertise<geometry_msgs::PointStamped>("/clicked_point_with_elevation", 10);
-    sub_ = nh_.subscribe("/clicked_point", 1, &GoalShifter::callback, this);
+    //sub_ = nh_.subscribe("/clicked_point", 1, &GoalShifter::callback, this);
+    sub_ = nh_.subscribe("/move_base_simple/goal", 1, &GoalShifter::callback, this);
   }
-
-  void callback(const geometry_msgs::PointStamped::ConstPtr & msg)
+//void callback(const geometry_msgs::PointStamped::ConstPtr & msg)
+  void callback(const geometry_msgs::PoseStamped::ConstPtr & msg)
   {
     geometry_msgs::PointStamped elevated_goal;
-    geometry_msgs::PointStamped current_goal;
+    //geometry_msgs::PointStamped current_goal;
+    geometry_msgs::PoseStamped current_goal;
     current_goal = *msg;
-    elevated_goal.point.x = current_goal.point.x;
-    elevated_goal.point.y = current_goal.point.y;
-    elevated_goal.point.z = current_goal.point.z + 2.0;
+    elevated_goal.point.x = current_goal.pose.position.x;
+    elevated_goal.point.y = current_goal.pose.position.y;
+    elevated_goal.point.z = current_goal.pose.position.z + 2.0;
     elevated_goal.header = current_goal.header;
     pub_.publish(elevated_goal);
   }
